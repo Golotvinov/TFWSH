e54ce9d9ee2ce32c8b7825dc2a69e41b83d99107
Aleks Golotvinov <golotvinov@gmail.com>
2024-11-12 00:05:24 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>Welcome to My Nuxt App!</h1>
+    </div>
+</template>
+
+<script setup>
+// (optional) Scripts here
+</script>
+
+<style scoped>
+/* (optional) Styles here */
+</style>
